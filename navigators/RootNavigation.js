ef7516e4e45ff5ef1fe50da1dc38da79e8f8d85b
Tony Luo <luo.tony37@gmail.com>
2018-01-03 20:36:23 -0800

    Add android backbutton support

--- navigators/RootNavigation.js
+++ navigators/RootNavigation.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import { BackHandler } from 'react-native';
 import { connect } from 'react-redux';
-import { addNavigationHelpers } from 'react-navigation';
+import { addNavigationHelpers, NavigationActions } from 'react-navigation';
 import RootNavigator from './RootNavigator';
 import { initializeDeck } from '../actions';
 
 class RootNavigation extends React.Component {
   componentDidMount() {
     this.props.initializeDeck();
+    BackHandler.addEventListener('hardwareBackPress', this.onBackPress);
+  }
+
+  componentWillUnmount() {
+    BackHandler.removeEventListener('hardwareBackPress', this.onBackPress);
+  }
+
+  onBackPress = () => {
+    const { dispatch, nav } = this.props;
+    if (nav.index === 0) {
+      return false;
+    }
+    dispatch(NavigationActions.back());
+    return true;
   }
 
   render() {
